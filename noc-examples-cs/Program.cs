29470a241d66557c38eb503bb9d2a4b7285034fa
Luciano Teixeira Borges <luciano.borges@way2.com.br>
2022-02-13 21:16:55 -0300

    Initial setup

---
+++ noc-examples-cs/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace noc_examples_cs {
+    public static class Program {
+        [STAThread]
+        static void Main() {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
